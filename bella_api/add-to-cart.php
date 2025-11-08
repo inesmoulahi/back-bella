9663b15a4b3258adecddf2495e4194faf7a6523d
inesmoulahi <moulahimoulahi25@gmail.com>
2025-11-08 09:56:38 +0100

    Update add-to-cart.php

--- bella_api/add-to-cart.php
+++ bella_api/add-to-cart.php
@@ -3,7 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json");
 
-// Connexion à la base de données
 $host = "localhost";
 $user = "root";
 $password = "";
@@ -14,20 +13,14 @@ $conn = new mysqli($host, $user, $password, $dbname);
 if ($conn->connect_error) {
     die(json_encode(["success" => false, "message" => "Erreur de connexion"]));
 }
-
-// Lire les données JSON reçues depuis Angular
 $data = json_decode(file_get_contents("php://input"), true);
-
-// Simuler un utilisateur connecté (à remplacer plus tard)
 $user_id = 14;
-
 $pid = $conn->real_escape_string($data['id']);
 $name = $conn->real_escape_string($data['name']);
 $price = $data['price'];
 $image = $conn->real_escape_string($data['image']);
 $quantity = 1;
 
-// Vérifier si le produit est déjà dans le panier
 $check = $conn->query("SELECT * FROM cart WHERE user_id = $user_id AND pid = $pid");
 if ($check->num_rows > 0) {
     echo json_encode(["success" => false, "message" => "Produit déjà dans le panier"]);
@@ -44,3 +37,4 @@ if ($check->num_rows > 0) {
 
 $conn->close();
 ?>
+
